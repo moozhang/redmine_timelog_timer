2b0a29dce2f7bcea1eedc2e470cbe6c64150b169
e-dot <e@kartmann.org>
2019-04-25 17:12:51 +0200

    Create fr.js
    French translation

---
+++ assets/javascripts/i18n/fr.js
@@ -0,0 +1,12 @@
+$.timerlog_timer_i18n = {
+	minutes: ['{0} minute', '{0} minutes'],
+	hours: ['{0} heure', '{0} heures'],
+	title: 'Chronomètre',
+	activeTimer: 'Un chronomètre est en cours.',
+	actionMerge: 'Fusionner avec le chronomètre en cours',
+	remove: 'Supprimer le chronomètre en cours',
+	openForm: 'Ouvrir le formulaire du chronomètre en cours',
+	removeConfirm: 'Etes-vous sûr?',
+	start: 'Démarrer',
+	stop: 'Arrêter'
+};
