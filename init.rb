b0091b298828eb6a33986df6e3d671768098b4ef
e-dot <e@kartmann.org>
2019-04-25 17:18:29 +0200

    Update init.rb

--- init.rb
+++ init.rb
@@ -5,7 +5,7 @@ Redmine::Plugin.register :redmine_timelog_timer do
   name 'Timelog Timer plugin'
   author 'Behigh & Github Community'
   description 'A leightweight javascript timer for Hours field in timelog form'
-  version '2.0.1'
+  version '2.0.3'
   url 'https://github.com/behigh/redmine_timelog_timer.git'
 #  author_url 'http://example.com/about'
 end
