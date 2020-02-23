11a848fbd936f69c0d2deb22f6d8544d85846d78
moozhang <mzhang@nao.cas.cn>
2020-02-23 13:40:39 +0800

    add Chinese support

---
+++ assets/javascripts/i18n/zh.js
@@ -0,0 +1,12 @@
+$.timerlog_timer_i18n = {
+	minutes: ['{0} 分', '{0} 分'],
+	hours: ['{0} 小时', '{0} 小时'],
+	title: '计时器',
+	activeTimer: '已启动计时器.',
+	actionMerge: '合并到当前计时器',
+	remove: '停止并删除计时器',
+	openForm: '打开正在计时的页面',
+	removeConfirm: '确定删除计时器?',
+	start: '开始',
+	stop: '停止'
+};
