23d976520142dbc8cd397bdf678af6f137dbd13d
moozhang <mzhang@nao.cas.cn>
2020-02-23 13:40:23 +0800

    cleanup for selfuse

--- init.rb
+++ init.rb
@@ -3,9 +3,8 @@ require_dependency 'timelog_timer_hook_listener'
 
 Redmine::Plugin.register :redmine_timelog_timer do
   name 'Timelog Timer plugin'
-  author 'Behigh & Github Community'
+  author 'Behigh & github Comunity; Moded for selfuse by Mo'
   description 'A leightweight javascript timer for Hours field in timelog form'
-  version '2.0.3'
-  url 'https://github.com/e-dot/redmine_timelog_timer'
-#  author_url 'http://example.com/about'
+  version '3.0'
+  url 'https://github.com/moozhang/redmine_timelog_timer'
 end
